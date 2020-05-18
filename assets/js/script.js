d97914539dc4cc2eb4ec5d81ea537df91b7bcb22
1hoppy1 <jahopki@hotmail.com>
2020-05-17 21:09:18 -0600

    generator works

--- assets/js/script.js
+++ assets/js/script.js
@@ -132,104 +132,103 @@
                     writePassword();
                   }
                   else {
-                    // INSERT CODE GENERATOR FUNCTION HERE
+                    // LEAVING BLANK
                     alert("congrats jim")
-                    
                   }
                   
+
+                  var uC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                  var lC = "abcdefghijklmnopqrstuvwxyz";
+                  var wN = "0123456789";
+                  var sC = "!#$%&'()*+,-./:;<=>?@";
                   
                   if (
                     (upperCase === 'yes' || 
                     upperCase === 'y')) {
-                      var upperCase = "P";
+                      var upperCase = 8;
                       console.log(upperCase);
                     }
                   else {
-                      var upperCase = 0
+                      var uC = 0;
                       console.log(upperCase);
                   }
 
                   if (
                     (lowerCase === 'yes' || 
                     lowerCase === 'y')) {
-                      var lowerCase = "Q";
+                      var lowerCase = 8;
                       console.log(lowerCase);
                     }
                   else {
-                      var lowerCase = 0
+                      var lC = 0;
                       console.log(lowerCase);
                   }
 
                   if (
                     (wholeNumbers === 'yes' || 
                     wholeNumbers === 'y')) {
-                      var wholeNumbers = "R";
+                      var wholeNumbers = 8;
                       console.log(wholeNumbers);
                     }
                   else {
-                      var wholeNumbers = 0
+                      var wN = 0;
                       console.log(wholeNumbers);
                   }
 
                   if (
                     (specialCharacters === 'yes' || 
                     specialCharacters === 'y')) {
-                      var specialCharacters = "S";
+                      var specialCharacters = 8;
                       console.log(specialCharacters);
                     }
                   else {
-                      var specialCharacters = 0
+                      var sC = 0;
                       console.log(specialCharacters);
                   }        
                  
-     /*             
-                  var uC = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-                  var lC = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-                  var wN = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-                  var sC = ['!', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@']
-      */
      
-      
-//CODE WORKS UP TO HERE!!!
-
-        /*        var uC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-                  var lC = "abcdefghijklmnopqrstuvwxyz";
-                  var wN = "0123456789";
-                  var sC = "!#$%&'()*+,-./:;<=>?@";
-        */
-                  var char = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-                  var num = "0123456789";
-                  var sym = "!#$%&'()*+,-./:;<=>?@";
-
-                  var characters = char;
-                 // (numBox."R") ? characters += num : '';
-                 // (symBox."S") ? characters += sym : '';
-                 var charNum = passwordLength;
-                 // var numBox = "R";
-                  //var symBox = "S"; 
-                 
-                 
-                 
-                console.log(charNum, characters);
+                
         
-                console.log(passwordLength, characters);
+                console.log(passwordLength);
+// CODE WORKS TO HERE
 
-
-                  function password(l, char){
-                    
+                  function generatePassword(passwordLength, uC, lC, wN, sC){
+                  
+                    var charSet = "";
                     var pwd = "";
+                  
+                    if (upperCase === 8) {
+                      charSet = (uC + lC + wN + sC);
+                    }
+                    else {}
+                    if (lowerCase === 8) {
+                      charSet = (uC + lC + wN + sC);
+                    }
+                   else {}
+                   if (wholeNumbers === 8) {
+                    charSet = (uC + lC + wN + sC);
+                   }
+                   else {}
+                   if (specialCharacters === 8) {
+                    charSet = (uC + lC + wN + sC);
+                   }
+                   else{}
 
-                    for(var i = 0; i<l; i++) {
+                   console.log(charSet);                           
 
-                      pwd = pwd + char.charAt(Math.floor(Math.random() * Math.floor(char.length -1)));
-                      console.log(pwd)
+                    for (var i = 0; i<passwordLength; i++) {
+
+                      
+                      var pwd = pwd + charSet.charAt(Math.floor(Math.random() * 
+                      Math.floor(charSet.length - 1)));
+                      console.log(pwd);
                     }
                   
 
                     return pwd;
 
                   }
-                  password(passwordLength, char);             
+                  generatePassword(passwordLength, uC, lC, wN, sC);             
                 
                 
                 
@@ -281,3 +280,21 @@ var generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", function() {
   writePassword();
 });
+
+
+
+/*             
+                  var uC = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+                  var lC = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+                  var wN = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+                  var sC = ['!', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@']
+      */
+     
+      
+//CODE WORKS UP TO HERE!!!
+
+        /*        var uC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                  var lC = "abcdefghijklmnopqrstuvwxyz";
+                  var wN = "0123456789";
+                  var sC = "!#$%&'()*+,-./:;<=>?@";
+        */
